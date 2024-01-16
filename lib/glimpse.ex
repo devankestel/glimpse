7915a191383ae672be5859a97a8c27a1e122ae60
Devan Kestel <devan.kestel@gmail.com>
2024-01-15 16:36:17 -0800

    (feat): abstract dtypes into own method to prepare for non-integer file reads.

--- lib/glimpse.ex
+++ lib/glimpse.ex
@@ -36,21 +36,32 @@ defmodule Glimpse do
     Enum.join([header, footer])
   end
 
-  def read(path) do
-    dtypes = [
-      {"a", :integer},
-      {"b", :integer},
-      {"c", :integer},
-      {"d", :integer},
-      {"e", :integer},
-      {"f", :integer},
-      {"h", :integer},
-      {"i", :integer},
-      {"j", :integer},
-      {"k", :integer},
-      {"l", :integer},
-      {"m", :integer},
-      ]
+  def create_dtypes(column_names) do
+    column_names |> 
+      Enum.map(
+        fn column_name ->
+          {column_name, :integer}
+        end
+      )  
+  end 
+
+  def read(path, dtypes) do
     DF.from_csv!(path, dtypes: dtypes)
   end
+
+# notes 
+# cwd = File.cwd!
+# python_file_path = "#{cwd}/test/fixtures/test.py"
+# df = DF.from_csv!(python_file_path, delimiter: "\n", header: false)
+# df[0][0] |> String.codepoints
+# ["i", "m", "p", "o", "r", "t", " ", "p", "y", "t", "e", "s", "t"]
+# char_line = df[0][0] |> String.codepoints
+# char_line_series = Explorer.Series.from_list(char_line)
+# DF.new(%{line: char_line_series})
+# 1 string column named line with 13 rows each a character
+#Explorer.DataFrame<
+  # Polars[13 x 1]
+  # line string ["i", "m", "p", "o", "r", ...]
+# maybe try a mutation query
 end
+
--- test/glimpse_test.exs
+++ test/glimpse_test.exs
@@ -39,12 +39,72 @@ defmodule GlimpseTest do
     cwd = File.cwd!
     filepath = "test/fixtures/test.csv"
     path = "#{cwd}/#{filepath}"
-    result_dataframe = Glimpse.read(path)
+    dtypes =  [
+      {"a", :integer},
+      {"b", :integer},
+      {"c", :integer},
+      {"d", :integer},
+      {"e", :integer},
+      {"f", :integer},
+      {"g", :integer},
+      {"h", :integer},
+      {"i", :integer},
+      {"j", :integer},
+      {"k", :integer},
+      {"l", :integer},
+      {"m", :integer},
+    ]
+
+    result_dataframe = Glimpse.read(path, dtypes)
     # groups
     # names
     # data
     assert result_dataframe.dtypes == expected_dataframe.dtypes
     assert DF.to_columns(result_dataframe) == DF.to_columns(expected_dataframe)
 
+  end
+
+  test "create_dtypes for integers CSV" do
+    expected_dtypes = [
+      {"a", :integer},
+      {"b", :integer},
+      {"c", :integer},
+      {"d", :integer},
+      {"e", :integer},
+      {"f", :integer},
+      {"g", :integer},
+      {"h", :integer},
+      {"i", :integer},
+      {"j", :integer},
+      {"k", :integer},
+      {"l", :integer},
+      {"m", :integer},
+      ] 
+
+      column_names = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m"]
+
+      result_dtypes = Glimpse.create_dtypes(column_names)
+
+      assert result_dtypes == expected_dtypes
+  end
+
+  test "read in an integer dataframe integration test" do
+    expected_dataframe = DF.new([
+      %{a: 1, b: 1, c: 1, d: 0, e: 0, f: 0, g: 1, h: 1, i: 1, j: nil, k: nil, l: nil, m: nil},
+      %{a: 0, b: 0, c: 0, d: 1, e: 1, f: 0, g: 0, h: 1, i: 1, j: nil, k: nil, l: nil, m: nil},
+      %{a: 1, b: 1, c: 1, d: 1, e: 1, f: 1, g: 1, h: 1, i: 1, j: 1, k: 1, l: 1, m: 0}
+    ])
+    cwd = File.cwd!
+    filepath = "test/fixtures/test.csv"
+    path = "#{cwd}/#{filepath}"
+    column_names = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m"]
+
+    dtypes = Glimpse.create_dtypes(column_names)
+    result_dataframe = Glimpse.read(path, dtypes)
+    # groups
+    # names
+    # data
+    assert result_dataframe.dtypes == expected_dataframe.dtypes
+    assert DF.to_columns(result_dataframe) == DF.to_columns(expected_dataframe)
   end
 end
